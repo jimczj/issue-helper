3d0d84c5c3a7b2adcc8c78f43b6e8b6c31e1f726
jimczj <jimczj@gmail.com>
2019-01-23 13:18:13 +0800

    fix: 删除无用代码

--- src/app.js
+++ src/app.js
@@ -1,16 +1,11 @@
 import React from 'react'
-import { HashRouter as Router, Route } from 'react-router-dom'
 import Index from './pages/index/index.js'
 
 import './index.scss'
 
 class App extends React.Component {
   render () {
-    return (
-      <Router>
-        <Route path="/" exact component={Index} />
-      </Router>
-    )
+    return <Index/>
   }
 }
 
